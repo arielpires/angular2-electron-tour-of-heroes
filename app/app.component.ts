6d303ea3c9bd667ddfa2b61ef155b6e8fca6fd2b
Pando85 <pando855@gmail.com>
2017-01-18 00:11:31 +0100

    Not working routing

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,14 @@
 import { Component } from '@angular/core';
+
 @Component({
   selector: 'my-app',
-  template: '<h1>My First Angular 2 App with Electron</h1>'
+  template: `
+     <h1>{{title}}</h1>
+     <a routerLink="/heroes">Heroes</a>
+     <router-outlet></router-outlet>
+    `
 })
-export class AppComponent { }
+
+export class AppComponent {
+  title = 'Tour of Heroes';
+}
