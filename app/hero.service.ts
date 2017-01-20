296cd35b3c2563bef7361526571865a6417466d1
Pando85 <pando855@gmail.com>
2017-01-20 23:17:00 +0100

    Defined interface for backend

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -3,10 +3,19 @@ import { Injectable } from '@angular/core';
 import { Backend } from './http.backend';
 import { Hero } from './hero';
 
+interface BackendInterface {
+  getHeroes(): Promise<Hero[]>
+  getHero(id: number): Promise<Hero>
+  save(hero: Hero): Promise<Hero>
+  delete(hero: Hero): Promise<any>
+}
+
 @Injectable()
 export class HeroService {
 
-    backend: Backend;
+
+
+    backend: BackendInterface;
 
     constructor( backend: Backend ){
         this.backend = backend;
